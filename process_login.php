6ff4422799d4df84a51f8d88fd7918f2c97bac5b
jeremy-felipe <jeremyhonolulu@gmail.com>
2018-04-06 10:07:22 -1000

    update on formatting for login php things

--- process_login.php
+++ process_login.php
@@ -1,3 +1,26 @@
+<?php
+$dbServername = "db686893124.db.1and1.com";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "db686893124";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName) or die ("Error connecting to database.");
+
+$sql = "'SELECT username, password FROM users WHERE username=".$_POST["username"]." AND password=".$POST_["password"]."'";
+
+$result = mysqli_query($conn, $sql);
+$resultCheck = mysqli_num_rows($result);
+
+if ($resultCheck > 0) {
+    header('Location: https://www.bamboocalc.com/baboon-alert-system/index.html?error_login');
+}
+else {
+    //redirect back to login page
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,25 +33,7 @@
 </head>
 <body class="text-center">
 
-<?php
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "loginSystem";
 
-$conn = mysql_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-
-$sql = "'select username, password from users where username= ';";
-
-$result = mysqli_query($conn, $sql);
-$resultCheck = mysqli_num_rows($result);
-
-if ($resultCheck > 0) {
-
-}
-
-
->
 
 </body>
 
