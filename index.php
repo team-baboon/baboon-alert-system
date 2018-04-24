b8bf84d195898e90b126509b76e47dd4af1b8768
Iao Seng Sio <tonysio42@gmail.com>
2018-04-23 21:57:19 -1000

    Connect login to database

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+if (isset($_POST["login"])) {
+    $hostname = "db686893124.db.1and1.com";
+    $database = "db686893124";
+    $dbUsername = "dbo686893124";
+    $dbPassword = "*****";
+
+    $conn = mysqli_connect($hostname, $dbUsername, $dbPassword, $database) or die ("Error connecting to database.");
+
+    $sql = "SELECT username, password FROM users WHERE username='" . clean($_POST["username"]) . "' AND password='" . clean($_POST["password"]) . "'";
+
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+
+    if ($resultCheck === 1) {
+        header('location: menu.html');
+    } else {
+        $loginError = true;
+    }
+}
+
+function clean($data) {
+    return htmlspecialchars(stripslashes(trim($data)));
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Login</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link href="login.css" rel="stylesheet">
+</head>
+
+<body class="text-center">
+
+<form method="post" class="form-login">
+    <?php
+    if (isset($loginError)) {
+        echo '<div class="alert alert-danger">Invalid username or password.</div>';
+    }
+    ?>
+  <label for="username" class="sr-only">Username</label>
+  <input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
+  <label for="inputPassword" class="sr-only">Password</label>
+  <input type="password" name="password" class="form-control" placeholder="Password" required>
+  <button name="login" class="btn btn-lg btn-outline-primary btn-block" type="submit">Log in
+  </button>
+  <p class="mt-5 mb-3 text-muted"><a href="newuser.php">Create New User</a></p>
+  <p class="text-muted">ducey@hawaii.edu</p>
+</form>
+
+</body>
+
+</html>
